7d0be78df8229a1ba5ac83dae4be2d699e2872dd
Stevan Nikolic <stevannikolic1992@yahoo.com>
2024-04-16 20:11:04 +0200

    Added Redux Basic Setup

--- src/features/movies/MovieDetails.jsx
+++ src/features/movies/MovieDetails.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
   Box,
   Button,
@@ -13,11 +14,12 @@ import {
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Link, useParams } from "react-router-dom";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
-import { data } from "./data";
+import { selectMovieById } from "./moviesSlice";
 
 function MovieDetails() {
   const { movieId } = useParams();
-  const movie = data.results.find((movie) => movie.id === Number(movieId));
+  const movie = useSelector((state) => selectMovieById(state, movieId));
+
   return (
     <Box minH="100vh">
       <Link to="/">
@@ -41,14 +43,11 @@ function MovieDetails() {
           src={`${MOVIEDB_IMAGES_URL}/${movie.poster_path}`}
           alt={`${movie.title}'s poster`}
         />
-
         <Stack>
           <CardBody>
             <Heading size="md">{movie.title}</Heading>
-
             <Text py="2">{movie.overview}</Text>
           </CardBody>
-
           <CardFooter>
             <Button bg="green.300" color="white">
               Generate Joke
@@ -59,5 +58,4 @@ function MovieDetails() {
     </Box>
   );
 }
-
 export default MovieDetails;
